75f15bb2e2348d27de5fbc37c4a3b983ea931701
Farhan-07-00 <fakthar930@gmail.com>
2026-06-25 01:10:14 +0530

    Add Render port support

--- server.js
+++ server.js
@@ -66,8 +66,8 @@ app.post(
     }
 );
 
-app.listen(3000, () => {
-    console.log(
-        "Server running on http://localhost:3000"
-    );
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
